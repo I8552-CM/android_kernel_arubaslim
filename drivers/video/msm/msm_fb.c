3ecbf30dd9e7aeda6184e1e1f0a25c377ef34e9e
Manoj Rao <manojraj@codeaurora.org>
2013-04-12 18:37:14 -0700

    msm: msm_fb: remove mmio access through mmap
    Disable access to mm io and add
    appropriate range checks to ensure valid accesses
    through framebuffer mmap. This prevents illegal
    access into memory.
    
    CRs-Fixed: 474706
    Change-Id: If25166f2732433ef967e99c716440030b567aae9
    Signed-off-by: Manoj Rao <manojraj@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1387,13 +1387,19 @@ static int msm_fb_mmap(struct fb_info *info, struct vm_area_struct * vma)
 	unsigned long off = vma->vm_pgoff << PAGE_SHIFT;
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
 	msm_fb_pan_idle(mfd);
-	if ((vma->vm_end <= vma->vm_start) || (off >= len) ||
-		((vma->vm_end - vma->vm_start) > (len - off)))
+        if (!start)
 		return -EINVAL;
 
+	if ((vma->vm_end <= vma->vm_start) ||
+	    (off >= len) ||
+	    ((vma->vm_end - vma->vm_start) > (len - off)))
+		return -EINVAL;
 	/* Set VM flags. */
 	start &= PAGE_MASK;
 	off += start;
+        if (off < start)
+		return -EINVAL;
+
 	vma->vm_pgoff = off >> PAGE_SHIFT;
 	/* This is an IO map - tell maydump to skip this VMA */
 	vma->vm_flags |= VM_IO | VM_RESERVED;
