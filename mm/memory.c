e8f8e4ede64af278c6cb68e80d1033d270ec91d8
Alin <alin.p32@gmail.com>
2014-02-06 23:16:36 +0200

    mm: upgrade KSM module to Ultra KSM v0.122
    Advantages of this patch:
     - faster page scanning
     - higher succes rate of de-duplication
     - less CPU hungry daemon
     - customizable parameters through governors:
       - high
       - medium
       - low
       - quiet [default]
    
    Now the daemon stays mostly on 0% CPU usage and rarely reaches 1-3% of CPU usage.
    Those bursts of CPU usage don't affect the battery as they stay like this only for a few millisecs,
    when the daemon finds pages that need de-duplication.
    
    Change-Id: I45ebe25e772a73e7e3f3e66f409e18c79061916e

--- mm/memory.c
+++ mm/memory.c
@@ -115,6 +115,37 @@ __setup("norandmaps", disable_randmaps);
 unsigned long zero_pfn __read_mostly;
 unsigned long highest_memmap_pfn __read_mostly;
 
+#ifdef CONFIG_UKSM
+unsigned long uksm_zero_pfn __read_mostly;
+struct page *empty_uksm_zero_page;
+
+static int __init setup_uksm_zero_page(void)
+{
+  unsigned long addr;
+  addr = __get_free_pages(GFP_KERNEL | __GFP_ZERO, 0);
+  if (!addr)
+   panic("Oh boy, that early out of memory?");
+
+  empty_uksm_zero_page = virt_to_page((void *) addr);
+  SetPageReserved(empty_uksm_zero_page);
+
+  uksm_zero_pfn = page_to_pfn(empty_uksm_zero_page);
+
+  return 0;
+}
+core_initcall(setup_uksm_zero_page);
+
+static inline int is_uksm_zero_pfn(unsigned long pfn)
+{
+  return pfn == uksm_zero_pfn;
+}
+#else
+static inline int is_uksm_zero_pfn(unsigned long pfn)
+{
+  return 0;
+}
+#endif
+
 /*
  * CONFIG_MMU architectures set up ZERO_PAGE in their paging_init()
  */
@@ -727,8 +758,10 @@ static inline int is_cow_mapping(vm_flags_t flags)
 #ifndef is_zero_pfn
 static inline int is_zero_pfn(unsigned long pfn)
 {
-	return pfn == zero_pfn;
+	return (pfn == zero_pfn) || (is_uksm_zero_pfn(pfn));
 }
+#else
+#define is_zero_pfn(pfn)   (is_zero_pfn(pfn) || is_uksm_zero_pfn(pfn))
 #endif
 
 #ifndef my_zero_pfn
@@ -914,6 +947,10 @@ copy_one_pte(struct mm_struct *dst_mm, struct mm_struct *src_mm,
 			rss[MM_ANONPAGES]++;
 		else
 			rss[MM_FILEPAGES]++;
+	/* Should return NULL in vm_normal_page() */
+		    uksm_bugon_zeropage(pte);
+		  } else {
+		    uksm_map_zero_page(pte);
 	}
 
 out_set_pte:
@@ -1149,8 +1186,10 @@ again:
 			ptent = ptep_get_and_clear_full(mm, addr, pte,
 							tlb->fullmm);
 			tlb_remove_tlb_entry(tlb, pte, addr);
-			if (unlikely(!page))
+			if (unlikely(!page)) {
+				uksm_unmap_zero_page(ptent);
 				continue;
+			}
 			if (unlikely(details) && details->nonlinear_vma
 			    && linear_page_index(details->nonlinear_vma,
 						addr) != page->index)
@@ -2550,8 +2589,10 @@ static inline void cow_user_page(struct page *dst, struct page *src, unsigned lo
 			clear_page(kaddr);
 		kunmap_atomic(kaddr);
 		flush_dcache_page(dst);
-	} else
+	} else {
 		copy_user_highpage(dst, src, va, vma);
+		uksm_cow_page(vma, src);
+	}
 }
 
 /*
@@ -2756,6 +2797,7 @@ gotten:
 		new_page = alloc_zeroed_user_highpage_movable(vma, address);
 		if (!new_page)
 			goto oom;
+		uksm_cow_pte(vma, orig_pte);
 	} else {
 		new_page = alloc_page_vma(GFP_HIGHUSER_MOVABLE, vma, address);
 		if (!new_page)
@@ -2777,8 +2819,11 @@ gotten:
 				dec_mm_counter_fast(mm, MM_FILEPAGES);
 				inc_mm_counter_fast(mm, MM_ANONPAGES);
 			}
-		} else
-			inc_mm_counter_fast(mm, MM_ANONPAGES);
+		uksm_bugon_zeropage(orig_pte);
+	} else {
+		uksm_unmap_zero_page(orig_pte);
+		inc_mm_counter_fast(mm, MM_ANONPAGES);
+	}
 		flush_cache_page(vma, address, pte_pfn(orig_pte));
 		entry = mk_pte(new_page, vma->vm_page_prot);
 		entry = maybe_mkwrite(pte_mkdirty(entry), vma);
