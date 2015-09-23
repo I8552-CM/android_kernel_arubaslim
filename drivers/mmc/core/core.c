0115f6d614f8cb6bf99ca838a8599f023edba021
srfarias <ericfari15@gmail.com>
2015-09-23 14:35:51 -0300

    Squashed update of kernel from 3.4.108 to 3.4.109
    Conflicts:
    	arch/arm/configs/u8833_defconfig
    	drivers/mmc/core/core.c

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2624,6 +2624,20 @@ int mmc_pm_notify(struct notifier_block *notify_block,
 	switch (mode) {
 	case PM_HIBERNATION_PREPARE:
 	case PM_SUSPEND_PREPARE:
+<<<<<<< HEAD
+=======
+	case PM_RESTORE_PREPARE:
+		if (host->card && mmc_card_mmc(host->card)) {
+			mmc_claim_host(host);
+			err = mmc_stop_bkops(host->card);
+			mmc_release_host(host);
+			if (err) {
+				pr_err("%s: didn't stop bkops\n",
+					mmc_hostname(host));
+				return err;
+			}
+		}
+>>>>>>> 4b7ca0a... Squashed update of kernel from 3.4.108 to 3.4.109
 
 		spin_lock_irqsave(&host->lock, flags);
 		if (mmc_bus_needs_resume(host)) {
