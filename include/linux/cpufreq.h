e65905ffa677ba06fe9e26c74411485fd5c1e4ae
Grace5921 <androidlover5842@gmail.com>
2016-09-14 20:30:01 +0530

    cpufreq: LIONHEART  cpu freqency governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -435,6 +435,9 @@ extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionheart)
 #endif
 
 /*********************************************************************
