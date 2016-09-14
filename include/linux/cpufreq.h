8e561d88a36e27e5c384b4777773001ed2d9e81a
Grace5921 <androidlover5842@gmail.com>
2016-09-14 20:31:18 +0530

    cpufreq: ONDEMANDX  cpu freqency governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -438,6 +438,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionheart)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
+extern struct cpufreq_governor cpufreq_gov_ondemandx;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandx)
 #endif
 
 /*********************************************************************
