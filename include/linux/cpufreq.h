b08c3d46602894d988d5ccc432d9d2bae4893cbf
Grace5921 <androidlover5842@gmail.com>
2016-09-14 22:43:36 +0530

    cpufreq: SMOOVE  cpu freqency governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -447,6 +447,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMOOVE)
+extern struct cpufreq_governor cpufreq_gov_smoove;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smoove)
 #endif
 
 /*********************************************************************
