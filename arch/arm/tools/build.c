2ff749521597f9b6a94150f83d07de32083612c3
Grace5921 <androidlover5842@gmail.com>
2016-09-15 19:10:53 +0530

    build: use c language instead of shell script to fix adb issue while flashing

---
+++ arch/arm/tools/build.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+void load_menu(void);
+void Compile(void);
+void Flash(void);
+void Clean(void);
+int main(int argc, char** argv)
+{
+    load_menu();
+    return 0;
+}
+ 
+void load_menu(void)
+{
+    int choice;
+ 
+    do
+    {
+   system("echo 'MSM7627a'");
+        printf("1. Compile\n");
+        printf("2. Flash\n");
+	printf("3. Clean\n");
+        printf("4. Exit\n");
+        scanf("%d",&choice);
+ 
+        switch(choice)
+        {
+            case 1: Compile();
+                break;
+            case 2: Flash();
+                break;
+	    case 3: Clean();
+		break;
+            case 4: printf("Quitting program!\n");
+		system("rm build");
+                exit(0);
+                break;
+            default: printf("Invalid choice!\n");
+                break;
+        }
+ 
+    } while (choice != 3);
+ 
+}
+ 
+void Compile(void)
+{
+    		system("chmod 775 arch/arm/tools/scripts/compile; ./arch/arm/tools/scripts/compile");
+
+}     
+
+void Flash(void)
+{
+
+    		system("chmod 775 arch/arm/tools/scripts/flash; ./arch/arm/tools/scripts/flash");
+return;
+}
+void Clean(void)
+{
+    		system("make mrproper");
+return;
+}
+
