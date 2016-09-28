53545a27c32be5739c3c2cb613b9534fd1684a0f
Grace5921 <androidlover5842@gmail.com>
2016-09-28 18:53:39 +0530

    Implement Kexec-hardboot

--- arch/arm/mach-msm/board-msm7x27a.c
+++ arch/arm/mach-msm/board-msm7x27a.c
@@ -42,6 +42,9 @@
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <asm/hardware/gic.h>
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <asm/setup.h>
+#endif
 #include <mach/board.h>
 #include <mach/msm_iomap.h>
 #include <mach/msm_hsusb.h>
@@ -2592,8 +2595,19 @@ static void __init msm7627a_reserve(void)
 #endif
 }
 
+
 static void __init msm8625_reserve(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+    // Reserve space for hardboot page at the end of first system ram block
+    struct membank* bank = &meminfo.bank[0];
+    phys_addr_t start = bank->start + bank->size - SZ_1M;
+    int ret = memblock_remove(start, SZ_1M);
+   if(!ret)
+        pr_info("Hardboot page reserved at 0x%X\n", start);
+    else
+        pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
 	memblock_remove(MSM8625_NON_CACHE_MEM, SZ_2K);
 	memblock_remove(MSM8625_CPU_PHYS, SZ_8);
 	memblock_remove(MSM8625_WARM_BOOT_PHYS, SZ_32);
--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -35,6 +35,10 @@
 #include "msm_watchdog.h"
 #include "timer.h"
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <asm/kexec.h>
+#endif
+
 #define WDT0_RST	0x38
 #define WDT0_EN		0x40
 #define WDT0_BARK_TIME	0x4C
@@ -251,6 +255,23 @@ static int __init msm_pmic_restart_init(void)
 
 late_initcall(msm_pmic_restart_init);
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+static void msm_kexec_hardboot_hook(void)
+{
+#if defined(CONFIG_MSM_DLOAD_MODE) && !defined(CONFIG_SEC_DEBUG)
+    /* Do not enter download mode on reboot. */
+    set_dload_mode(0);
+#endif
+
+    // Set PMIC to restart-on-poweroff
+    pm8xxx_reset_pwr_off(1);
+
+    /* Reboot with the recovery kernel since the boot kernel decompressor may
+     * not support the hardboot jump. */
+    __raw_writel(0x77665502, restart_reason);
+}
+#endif
+
 static int __init msm_restart_init(void)
 {
 #ifdef CONFIG_MSM_DLOAD_MODE
@@ -261,6 +282,10 @@ static int __init msm_restart_init(void)
 	msm_tmr0_base = msm_timer_get_timer0_base();
 	restart_reason = MSM_IMEM_BASE + RESTART_REASON_ADDR;
 	pm_power_off = msm_power_off;
+
+#ifdef CONFIG_KEXEC_HARDBOOT
+    kexec_hardboot_hook = msm_kexec_hardboot_hook;
+#endif
 
 	return 0;
 }
